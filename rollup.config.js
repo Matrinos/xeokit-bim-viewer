16efd3104925e8fb81d0c974612ca2c7b655b75d
Q.s <wangqs_eclipse@yahoo.com>
2021-09-30 11:20:01 +0800

    Add babel source map support with correct line number

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,25 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import {terser} from "rollup-plugin-terser";
+import nodeResolve from "rollup-plugin-node-resolve";
+// import { terser } from "rollup-plugin-terser";
+import { babel } from '@rollup/plugin-babel';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
-    input: './index.js',
-    output: {
-        file: './app/xeokit-bim-viewer.es.js',
-        format: 'es',
-        name: 'bundle',
-        sourcemap: 'inline',
-    },
-    plugins: [
-        nodeResolve(),
-         terser()
-    ]
-}
+  input: "./index.js",
+  output: {
+    file: "./app/xeokit-bim-viewer.es.js",
+    format: "es",
+    name: "bundle",
+    sourcemap: true,
+  },
+  plugins: [
+    sourcemaps(),
+    nodeResolve(),
+    // terser(),
+    babel({
+      inputSourceMap: false,
+      babelrc: true,
+      // babelHelpers: 'bundled',
+      presets: [["@babel/env", { modules: false }]],
+    }),
+  ],
+};
